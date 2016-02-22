d5c79cffae9e1eff08f023fffc4be3186f98933a
m.cubitt <m.cubitt@marketinvoice.com>
2016-02-22 18:49:37 +0000

    first commit. react, koa, jwt

---
+++ server.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var fs = require('fs');
+var koa = require('koa');
+var parse = require('co-body');
+var jwt = require('koa-jwt');
+var serve = require('koa-static');
+var bodyParser = require('koa-bodyparser');
+var router = require('koa-router')({
+    prefix: '/api'
+});
+
+var app = koa();
+
+app.use(serve('.'));
+app.use(bodyParser());
+
+var publicKey = fs.readFileSync('demo.rsa.pub');
+var privateKey = fs.readFileSync('demo.rsa');
+
+app.use(function *(next) {
+    try {
+        yield next; //Attempt to go through the JWT Validator
+    } catch(e) {
+        if (e.status == 401 ) {
+            // Prepare response to user.
+            this.status = e.status;
+            this.body = 'You don\'t have a signed token dude :('
+        } else {
+            throw e; // Pass the error to the next handler since it wasn't a JWT error.
+        }
+    }
+});
+
+app.use(function *(next) {
+    if (this.url.match(/^\/login/)) {
+        var claims = yield parse(this);
+        var token = jwt.sign(claims, privateKey, {algorithm: 'RS256'});
+        this.status = 200;
+        this.body = {token: token};
+    } else {
+        yield next;
+    }
+});
+
+app.use(jwt({
+    secret: publicKey,
+    algorithm: 'RS256'
+}));
+
+var fixtures = [{
+    homeTeam: {
+        fullName: 'Germany',
+        shortName: 'GER'
+    },
+    awayTeam: {
+        fullName: 'England',
+        shortName: 'ENG'
+    }
+}, {
+    homeTeam: {
+        fullName: 'Italy',
+        shortName: 'ITA'
+    },
+    awayTeam: {
+        fullName: 'Spain',
+        shortName: 'SPA'
+    }
+}, {
+    homeTeam: {
+        fullName: 'Germany',
+        shortName: 'GER'
+    },
+    awayTeam: {
+        fullName: 'Spain',
+        shortName: 'SPA'
+    }
+}];
+
+router.get('/fixtures', function* (next) {
+    this.body = fixtures;
+
+    yield next;
+});
+
+router.post('/fixtures', function* (next) {
+    var fixture = this.request.body;
+
+    fixtures.push(fixture);
+
+    this.body = fixture;
+
+    yield next;
+});
+
+app.use(router.routes());
+
+app.listen(3333);
