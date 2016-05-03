4b693cbe2a8a7b1a4a4bd6233b51458f7f211bfe
Matt <m.cubitt>
2016-05-03 22:43:00 +0100

    fixed auth try

--- app/server/auth/authMiddleware.js
+++ app/server/auth/authMiddleware.js
@@ -4,28 +4,30 @@ var mongo = require('../mongo');
 var UserService = require('../users/userService');
 
 module.exports = function*(next){
+    var token = this.request.header.authorization;
+
+    var options = {
+        audience: authConfig.audience,
+        issuer: authConfig.issuer
+    };
+
+    var claims;
+
     try {
-        var token = this.request.header.authorization;
-
-        var options = {
-            audience: authConfig.audience,
-            issuer: authConfig.issuer
-        };
-
-        var claims = jwt.verify(token, authConfig.privateKey, options);
-
-        var currentUser = yield new UserService(mongo.db).find(claims.email);
-
-        this.currentUser = {
-            _id: currentUser._id,
-            email: currentUser.email,
-            name: currentUser.name,
-            role: currentUser.role
-        };
-
-        yield next;
+        claims = jwt.verify(token, authConfig.privateKey, options);
     } catch(ex) {
         this.status = 403;
         this.body = 'Forbidden';
     }
+
+    var currentUser = yield new UserService(mongo.db).find(claims.email);
+
+    this.currentUser = {
+        _id: currentUser._id,
+        email: currentUser.email,
+        name: currentUser.name,
+        role: currentUser.role
+    };
+
+    yield next;
 };
