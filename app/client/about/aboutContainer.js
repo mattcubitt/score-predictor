20c296e0ab81bbfdbc9e9532e1ff9a072506347b
Matt <m.cubitt>
2016-06-03 19:39:29 +0100

    jpegs

--- app/client/about/aboutContainer.js
+++ app/client/about/aboutContainer.js
@@ -23,7 +23,7 @@ class AboutContainer extends Component {
                         <p>Chairman, CFO and chief button presser</p>
                     </div>
                     <div className="col-md-4 text-xs-center">
-                        <img className="team-member" src="/assets/images/team/matt.png" alt="Matt"/>
+                        <img className="team-member" src="/assets/images/team/matt.jpg" alt="Matt"/>
                         <h4>Matt</h4>
                         <p>Chief architect and table foosball champion</p>
                     </div>
@@ -38,7 +38,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://facebook.github.io/react/">
-                                <img className="card-img-top" src="/assets/images/tech/react.png" alt="React"/>
+                                <img className="card-img-top" src="/assets/images/tech/react.jpg" alt="React"/>
                                 <div className="card-block">
                                     <h4 className="card-title">React</h4>
                                     <p className="card-text">Facebook's UI framework used to create dynamic web applications</p>
@@ -49,7 +49,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="http://redux.js.org/">
-                                <img className="card-img-top" src="/assets/images/tech/redux.png" alt="Redux"/>
+                                <img className="card-img-top" src="/assets/images/tech/redux.jpg" alt="Redux"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Redux</h4>
                                     <p className="card-text">Functional state container inspired by Elm.</p>
@@ -60,7 +60,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://nodejs.org/en/">
-                                <img className="card-img-top image-fix" src="/assets/images/tech/nodejs.png" alt="Node"/>
+                                <img className="card-img-top image-fix" src="/assets/images/tech/nodejs.jpg" alt="Node"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Node</h4>
                                     <p className="card-text">Server side javascript framework run on top of google's V8 engine which powers chrome.</p>
@@ -71,7 +71,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="http://koajs.com/">
-                                <img className="card-img-top" src="/assets/images/tech/koa.png" alt="koa"/>
+                                <img className="card-img-top" src="/assets/images/tech/koa.jpg" alt="koa"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Koa</h4>
                                     <p className="card-text">Nodejs web framework utilising generator functions. Made by the express team.</p>
@@ -82,7 +82,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://www.mongodb.com/">
-                                <img className="card-img-top" src="/assets/images/tech/mongodb.png" alt="mongodb"/>
+                                <img className="card-img-top" src="/assets/images/tech/mongodb.jpg" alt="mongodb"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Mongo DB</h4>
                                     <p className="card-text">NoSql json document store.</p>
@@ -93,7 +93,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://www.heroku.com/home">
-                                <img className="card-img-top" src="/assets/images/tech/heroku.png" alt="Heroku"/>
+                                <img className="card-img-top" src="/assets/images/tech/heroku.jpg" alt="Heroku"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Heroku</h4>
                                     <p className="card-text">Platform-as-a-Service cloud platform running nodejs among others. </p>
@@ -104,7 +104,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://webpack.github.io/">
-                                <img className="card-img-top image-fix" src="/assets/images/tech/webpack.png" alt="Webpack"/>
+                                <img className="card-img-top image-fix" src="/assets/images/tech/webpack.jpg" alt="Webpack"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Webpack</h4>
                                     <p className="card-text">Javascript module loader and bundler.</p>
@@ -115,7 +115,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="https://babeljs.io/">
-                                <img className="card-img-top" src="/assets/images/tech/babel.png" alt="Babel"/>
+                                <img className="card-img-top" src="/assets/images/tech/babel.jpg" alt="Babel"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Babel</h4>
                                     <p className="card-text">Javascript compiler supporting future versions ecmascript.</p>
@@ -126,7 +126,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="http://v4-alpha.getbootstrap.com/">
-                                <img className="card-img-top" src="/assets/images/tech/bootstrap.png" alt="Axure"/>
+                                <img className="card-img-top" src="/assets/images/tech/bootstrap.jpg" alt="Axure"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Bootstrap</h4>
                                     <p className="card-text">CSS framework for building adaptive and mobile first user interfaces.</p>
@@ -137,7 +137,7 @@ class AboutContainer extends Component {
                     <div className="col-md-3">
                         <div className="card">
                             <a target="_blank" href="http://www.axure.com/">
-                                <img className="card-img-top" src="/assets/images/tech/axure.png" alt="Axure"/>
+                                <img className="card-img-top" src="/assets/images/tech/axure.jpg" alt="Axure"/>
                                 <div className="card-block">
                                     <h4 className="card-title">Axure</h4>
                                     <p className="card-text">Prototyping and wireframing UI tool</p>
