6cf1bf8cbdfc1a5d94854e9f07806a1ce94032bc
Matt <m.cubitt>
2016-06-08 23:23:26 +0100

    tooltips ans wording (reverted from commit a131251a2200d7d892aefd7fd6f1e1c8b70fea17)

---
+++ app/client/fixtures/fixturesContainer.js
@@ -0,0 +1,270 @@
+import React, { Component, PropTypes, CSSTransitionGroup } from 'react';
+import request from 'axios';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import RoundSelector from '../roundSelector/roundSelector';
+import RoundLeaderTable from '../leaderTable/roundLeaderTable';
+import WildcardSelector from './wildcardSelector';
+import FixtureGrid from './fixtureGrid';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Transition } from 'react-motion-ui-pack';
+import { spring } from 'react-motion';
+import { Bounce } from 'react-motion-pack';
+
+class FixturesContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.autoSaveDebounce = _.debounce(this.onAutoSave, 1000);
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        return dispatch(this.fetchState());
+    }
+
+    fetchState() {
+        return dispatch => {
+            this.loadState(dispatch, '/predictions', 'LOAD_PREDICTIONS');
+            this.loadState(dispatch, '/leaderTables', 'LOAD_LEADER_TABLES');
+            this.loadState(dispatch, '/rounds', 'LOAD_ROUNDS');
+            this.setActiveRoute(dispatch);
+        };
+    }
+
+    setActiveRoute(dispatch) {
+        dispatch({
+            type: 'SET_ACTIVE_ROUTE',
+            route: 'FIXTURES'
+        });
+    }
+
+    loadState(dispatch, resource, type) {
+        return request(resource)
+            .then(response => dispatch({
+                type: type,
+                data: response.data
+            }));
+    }
+
+    onPredictionChange(prediction, score, property) {
+        const { dispatch } = this.props;
+
+        var scoreInt = parseInt(score);
+
+        if((score.length > 2 || isNaN(scoreInt) || scoreInt < 0) && score !== '') {
+            score = prediction[property];
+        }
+
+        this.autoSave();
+
+        return dispatch({
+            type: 'UPDATE_PREDICTION',
+            id: prediction._id,
+            score,
+            property
+        });
+    }
+
+    autoSave() {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'STARTED_AUTOSAVE'
+        });
+
+        this.autoSaveDebounce();
+    }
+
+    onAutoSave() {
+        const { dispatch, predictions } = this.props;
+        
+        return request('/predictions', {
+            method: 'post',
+            data: predictions
+        })
+        .then(() => dispatch({
+            type: 'FINISHED_AUTOSAVE'
+        }));
+    }
+
+    onPreviousRoundClick() {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'PREVIOUS_ROUND'
+        })
+    }
+
+    onNextRoundClick() {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'NEXT_ROUND'
+        })
+    }
+
+    onShowWildcardSelector(prediction) {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'UPDATE_PREDICTION_WILDCARDLOADING',
+            id: prediction._id,
+            loading: true
+        });
+
+        return request('/wildcards', {
+            method: 'get'
+        })
+        .then((response) => {
+            dispatch({
+                type: 'UPDATE_PREDICTION_WILDCARDLOADING',
+                id: prediction._id,
+                loading: false
+            });
+
+            dispatch({
+                type: 'OPEN_WILDCARD_SELECTOR',
+                prediction,
+                wildcards: response.data
+            });
+        });
+    }
+
+    onCloseWildcardSelector() {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'CLOSE_WILDCARD_SELECTOR'
+        });
+    }
+
+    onSaveWildcardSelector() {
+        const { dispatch, wildcardSelector } = this.props;
+
+        const selectedWildcard = wildcardSelector.wildcards.filter(w => w.selected)[0];
+        const predictionId = wildcardSelector.predictionId;
+
+        if(selectedWildcard !== undefined) {
+            return this.saveWildcard(dispatch, predictionId, selectedWildcard);
+        }
+
+        return this.deleteWildcard(dispatch, predictionId, selectedWildcard);
+    }
+
+    saveWildcard(dispatch, predictionId, selectedWildcard) {
+        return request(`/predictions/${predictionId}/wildcards`, {
+            method: 'post',
+            data: selectedWildcard
+        })
+        .then(() => {
+            dispatch({
+                type: 'SAVE_PREDICTION_WILDCARD',
+                predictionId,
+                selectedWildcard
+            });
+
+            dispatch({
+                type: 'CLOSE_WILDCARD_SELECTOR'
+            });
+        });
+    }
+
+    deleteWildcard(dispatch, predictionId, selectedWildcard) {
+        return request(`/predictions/${predictionId}/wildcards`, {
+            method: 'delete'
+        })
+        .then(() => {
+            dispatch({
+                type: 'SAVE_PREDICTION_WILDCARD',
+                predictionId,
+                selectedWildcard
+            });
+
+            dispatch({
+                type: 'CLOSE_WILDCARD_SELECTOR'
+            });
+        });
+    }
+
+    onSelectWildcard(wildcard) {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'SELECT_WILDCARD',
+            wildcard
+        })
+    }
+
+    render() {
+        const { predictions, autoSaving, rounds, leaderTables, wildcardSelector } = this.props;
+
+        const currentRoundId = rounds.current ? rounds.current._id : null;
+        const currentRoundName = rounds.current ? rounds.current.name : '';
+        const currentPredictions = predictions
+            .filter(p => p.fixture.roundId === currentRoundId);
+        const foundLeaderTables = leaderTables
+            .filter(t => t.roundId === currentRoundId);
+        const currentLeaderTable = foundLeaderTables.length === 0 ? undefined : foundLeaderTables[0];
+
+        const gridProps = {
+            currentPredictions,
+            currentRoundId,
+            autoSaving,
+            onPredictionChange: this.onPredictionChange.bind(this),
+            onShowWildcardSelector: this.onShowWildcardSelector.bind(this)
+        };
+
+        return (
+            <div>
+                <WildcardSelector
+                    wildcardSelector={wildcardSelector}
+                    onSaveWildcardSelector={this.onSaveWildcardSelector.bind(this)}
+                    onCloseWildcardSelector={this.onCloseWildcardSelector.bind(this)}
+                    onSelectWildcard={this.onSelectWildcard.bind(this)}
+                />
+                <div className="row">
+                    <div className="col-xs-12 text-xs-center">
+                        <RoundSelector rounds={rounds}
+                                       onPreviousRoundClick={this.onPreviousRoundClick.bind(this)}
+                                       onNextRoundClick={this.onNextRoundClick.bind(this)}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-12 col-md-8">
+                        <FixtureGrid {...gridProps} key={currentRoundName} />
+                    </div>
+                    <div className="col-xs-12 col-md-4">
+                        <RoundLeaderTable leaderTable={currentLeaderTable} roundName={currentRoundName} key={currentRoundName}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+FixturesContainer.defaultProps = {
+    wildcardSelector: { wildcards: [] }
+};
+
+
+FixturesContainer.propTypes = {
+    predictions: PropTypes.array.isRequired,
+    autoSaving: PropTypes.bool.isRequired,
+    rounds: PropTypes.object.isRequired,
+    leaderTables: PropTypes.array.isRequired,
+    wildcardSelector: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        predictions: state.predictions,
+        autoSaving: state.autoSaving,
+        rounds: state.rounds,
+        leaderTables: state.leaderTables,
+        wildcardSelector: state.wildcardSelector
+    }
+}
+
+export default connect(mapStateToProps)(FixturesContainer)
--- app/client/fixtures/team.js
+++ app/client/fixtures/team.js
@@ -9,66 +9,13 @@ export default class Team extends Component {
         return '/assets/images/flags/default.png'
     }
 
-    getTeamName(team) {
-        switch(team) {
-            case 'FRA':
-                return 'France';
-            case 'ROU':
-                return 'Romania';
-            case 'ALB':
-                return 'Albania';
-            case 'SUI':
-                return 'Switzerland';
-            case 'WAL':
-                return 'Wales';
-            case 'SVK':
-                return 'Slovakia';
-            case 'ENG':
-                return 'England';
-            case 'RUS':
-                return 'Russia';
-            case 'TUR':
-                return 'Turkey';
-            case 'CRO':
-                return 'Croatia';
-            case 'POL':
-                return 'Poland';
-            case 'GER':
-                return 'Germany';
-            case 'ESP':
-                return 'Spain';
-            case 'CZE':
-                return 'Czech Republic';
-            case 'IRL':
-                return 'Republic of Ireland';
-            case 'BEL':
-                return 'Belgium';
-            case 'ITA':
-                return 'Italy';
-            case 'AUT':
-                return 'Austria';
-            case 'HUN':
-                return 'Hungary';
-            case 'POR':
-                return 'Portugal';
-            case 'ISL':
-                return 'Iceland';
-            case 'UKR':
-                return 'Ukraine';
-            case 'NIR':
-                return 'Northern Ireland';
-            default:
-                return team;
-        }
-    }
-
     render() {
         const { prediction } = this.props;
 
         return (
             <div className="teams-col">
                 <div className="team">
-                    <div className="flag" data-toggle="tooltip" data-placement="bottom" title={this.getTeamName(prediction.fixture.homeTeam)}>
+                    <div className="flag" data-toggle="tooltip" data-placement="bottom" title={prediction.fixture.homeTeam}>
                         <img className="flag-icon"
                              src={this.getTeamImageSrc(prediction.fixture.homeTeam)}>
                         </img>
@@ -77,7 +24,7 @@ export default class Team extends Component {
                 </div>
                 <div className="versus">vs</div>
                 <div className="team">
-                    <div className="flag" data-toggle="tooltip" data-placement="bottom" title={this.getTeamName(prediction.fixture.awayTeam)}>
+                    <div className="flag" data-toggle="tooltip" data-placement="bottom" title={prediction.fixture.awayTeam}>
                         <img className="flag-icon"
                              src={this.getTeamImageSrc(prediction.fixture.awayTeam)}>
                         </img>
