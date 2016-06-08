48a69422bb06b32441bae05232b9e9e760c58024
Matt <m.cubitt>
2016-06-08 23:26:22 +0100

    fixed up revert

--- app/client/standings/prizeBanner.js
+++ app/client/standings/prizeBanner.js
@@ -4,32 +4,35 @@ export default class PrizeBanner extends Component {
 
     render() {
         return (
-            <div className="prize-banner">
-                <div className="prize-message">Why you should take your predictions seriously!!!</div>
-                <div className="prize-container">
-                    <div className="row">
-                        <div className="col-xs-12 col-md-3 prize-item">
-                            <div className="prize-icon"><img src="assets/images/ipad.png" alt="1st Place"/></div>
-                            <div className="prize-title">1st Place</div>
-                            <div className="prize-description">The MI Euro 2016 champion wins an iPad mini (or equivalent in amazon or john lewis vouchers)</div>
-                        </div>
-                        <div className="col-xs-12 col-md-3 prize-item">
-                            <div className="prize-icon"><img src="assets/images/headphones.png" alt="2nd Place"/></div>
-                            <div className="prize-title">2nd Place</div>
-                            <div className="prize-description">The runner up wins a pair of Beats headphones (or equivalent in amazon or john lewis vouchers)</div>
-                        </div>
-                        <div className="col-xs-12 col-md-3 prize-item">
-                            <div className="prize-icon"><img src="assets/images/plane.png" alt="3rd Place"/></div>
-                            <div className="prize-title">3rd Place</div>
-                            <div className="prize-description">The best of the rest gets a extra day holiday</div>
-                        </div>
-                        <div className="col-xs-12 col-md-3 prize-item">
-                            <div className="prize-icon"><img src="assets/images/package.png" alt="Last place prize"/></div>
-                            <div className="prize-title">Last Place</div>
-                            <div className="prize-description">The worst predictor has to gift wrap the winners' prizes</div>
+            <div>
+                <div className="prize-banner">
+                    <div className="prize-message">Why you should take your predictions seriously!!!</div>
+                    <div className="prize-container">
+                        <div className="row">
+                            <div className="col-xs-12 col-md-3 prize-item">
+                                <div className="prize-icon"><img src="assets/images/ipad.png" alt="1st Place"/></div>
+                                <div className="prize-title">1st Place</div>
+                                <div className="prize-description">The MI Euro 2016 champion wins an iPad mini*</div>
+                            </div>
+                            <div className="col-xs-12 col-md-3 prize-item">
+                                <div className="prize-icon"><img src="assets/images/headphones.png" alt="2nd Place"/></div>
+                                <div className="prize-title">2nd Place</div>
+                                <div className="prize-description">The runner up wins a pair of Beats headphones*</div>
+                            </div>
+                            <div className="col-xs-12 col-md-3 prize-item">
+                                <div className="prize-icon"><img src="assets/images/plane.png" alt="3rd Place"/></div>
+                                <div className="prize-title">3rd Place</div>
+                                <div className="prize-description">The best of the rest gets a extra day holiday</div>
+                            </div>
+                            <div className="col-xs-12 col-md-3 prize-item">
+                                <div className="prize-icon"><img src="assets/images/package.png" alt="Last place prize"/></div>
+                                <div className="prize-title">Last Place</div>
+                                <div className="prize-description">The worst predictor has to gift wrap the winners' prizes</div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <p className="note-prize">*or equivalent in Amazon or John Lewis vouchers</p>
             </div>
         )
     }
