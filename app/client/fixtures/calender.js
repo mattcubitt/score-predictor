b332f4a32c58dfaf61f416ff2ffb39056e99c642
Matt <m.cubitt>
2016-06-11 14:46:01 +0100

    game in progress indicator

--- app/client/fixtures/calender.js
+++ app/client/fixtures/calender.js
@@ -5,28 +5,38 @@ export default class Calender extends Component {
     render() {
         const { prediction } = this.props;
 
+        if(prediction.editable) {
+            return (
+                <div className="calender-col">
+                    <div className="calender">
+                        <div className="time">{moment(prediction.fixture.startsOn).format('HH:mm')}</div>
+                        <div className="date">{moment(prediction.fixture.startsOn).format('DD MMM')}</div>
+                    </div>
+                </div>
+            )
+        }
+
+        if(prediction.fixture.homeScore === undefined && prediction.fixture.awayScore === undefined) {
+            return (
+                <div className="calender-col">
+                    <div className="game-in-progress"><span className="hidden-xs-down">Game in progress </span><img src="./assets/images/ripple.gif" alt="game in process"/></div>
+                </div>
+            )
+        }
+
         return (
             <div className="calender-col">
-                {
-                    prediction.editable
-                        ?
-                        <div className="calender">
-                            <div className="time">{moment(prediction.fixture.startsOn).format('HH:mm')}</div>
-                            <div className="date">{moment(prediction.fixture.startsOn).format('DD MMM')}</div>
-                        </div>
-                        :
-                        <div>
-                            <div className="prediction-input result">
-                                <div className="readonly">{prediction.fixture.homeScore}</div>
-                            </div>
-                            <div className="dash">
-                                <span>–</span>
-                            </div>
-                            <div className="prediction-input result">
-                                <div className="readonly">{prediction.fixture.awayScore}</div>
-                            </div>
-                        </div>
-                }
+                <div>
+                    <div className="prediction-input result">
+                        <div className="readonly">{prediction.fixture.homeScore}</div>
+                    </div>
+                    <div className="dash">
+                        <span>–</span>
+                    </div>
+                    <div className="prediction-input result">
+                        <div className="readonly">{prediction.fixture.awayScore}</div>
+                    </div>
+                </div>
             </div>
         )
     }
