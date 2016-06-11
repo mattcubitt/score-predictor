879f60a96999e9859e938756ced5ebbc58d3c8f5
Matt <m.cubitt>
2016-06-11 19:51:18 +0100

    added target blank

--- app/client/navigation/footer.js
+++ app/client/navigation/footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div style={{height: '100px', padding: '50px'}}>
-                <p className="text-xs-center text-muted">Icons by <a href="https://thenounproject.com/david.padrosa/">David Padrosa</a>.</p>
+                <p className="text-xs-center text-muted">Icons by <a href="https://thenounproject.com/david.padrosa/" target="_blank">David Padrosa</a>.</p>
             </div>
         )
     }
