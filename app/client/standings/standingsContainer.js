6f735129e04a8e9f664803c2e4bcbb0c76ac1060
Matt <m.cubitt>
2016-06-19 15:38:35 +0100

    showing round table 2

--- app/client/standings/standingsContainer.js
+++ app/client/standings/standingsContainer.js
@@ -64,7 +64,7 @@ class OverallStandingsContainer extends Component {
                             sortedRounds.map(round => {
                                 const leaderTable = leaderTables.filter(t => t.roundId === round._id)[0];
 
-                                if(round._id !== 1) {
+                                if(round._id !== 1 && round._id !== 2) {
                                     return(
                                         <div className="row" key={round._id}>
                                             <div className="col-xs-12">
